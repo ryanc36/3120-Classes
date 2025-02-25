7bfadcfc65b842d5cd6000fffc6dce1753b275b9
Andreasrubio97 <aldoramirez0897@gmail.com>
2025-02-24 19:45:20 -0500

    aldo changes

--- Animal.py
+++ Animal.py
@@ -1,8 +1,15 @@
 class Animal:
-    def __init__(self, name):
+    def __init__(self, name, walk):
         self.__name = name 
+        self.__walk = walk
         print("hello, I am", self.__name)
 
     def talk(self):
-        print("hi")
+        print(self.__name, "hi")
+
+    def walk(self):
+        print(self.__name, "is walking", self.__walk)
+
+    #def eat(self):
+        #print(f"{self.__name} is eating.")
 
